40d573cbf48c1422ee881eab525a7cfc7826424e
devVargas <alissonvargas97@gmail.com>
2025-01-15 17:55:42 -0300

    Inserção e limpeza do formulário

---
+++ dist/controllers/negociacao-controller.js
@@ -0,0 +1,21 @@
+import { Negociacao } from "../models/negociacao.js";
+export class NegociacaoController {
+    inputData;
+    inputQuantidade;
+    inputValor;
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+    }
+    criaNegociacao() {
+        const exp = /-/g;
+        const data = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(data, quantidade, valor);
+    }
+}
